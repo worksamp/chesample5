fde6b0d090e76bb4ef45bb786f3ec905c73d3c09
Daria Dementieva <ddementieva@codenvy.com>
2016-05-30 14:18:02 +0300

    UseUrls added

--- Program.cs
+++ Program.cs
@@ -12,6 +12,7 @@ namespace HelloWeb
                         .UseContentRoot(Directory.GetCurrentDirectory())
                         .UseIISIntegration()
                         .UseStartup<Startup>()
+                        .UseUrls("http://0.0.0.0:5000/")
                         .Build();
 
             host.Run();
